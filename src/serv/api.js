d2eb62be7ed93041df8effe76a1b974715f6d187
MTV <matveivadimovih@gmail.com>
2026-05-19 20:31:41 +0300

    Implement login and logout methods in ApiService; add user authorization checks

--- src/serv/api.js
+++ src/serv/api.js
@@ -1,13 +1,51 @@
+import { market } from "./market.js"
+
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 export class ApiService
 {
     constructor(balance = 10000)
     {
         this.userBalance = balance;
+        this.userPortfolio = new Map()
+        this.authToken = null;
+        this.tokenTimeTo = null;
+
+        this._subscribeMarketEvents()
     }
-    
-    getBalance()
+
+    async login(username, password)
     {
-        console.log("[API] get balance")
-        return {balance: this.userBalance};
+        await delay(200);
+
+        if(username === "user" && password === "123")
+        {
+            this.authToken = `token_${Date.now()}`
+            this.tokemTimeTo = Date.now() + 5 * 60 * 1000;
+            return {success: true, token: this.authToken};
+        }
+        throw new Error("Invalid username or password")
+    }
+
+    async logout()
+    {
+        await delay(200);
+        this.authToken = null;
+        this.tokenTimeTo = null;
+    }
+
+    isAuthorized()
+    {
+        return (this.authToken != null && Date.now() < this.tokenTimeTo);
+    }
+
+    async getStockPrice(symbol) 
+    {
+
+    }
+
+    async getAllPrices() 
+    {
+
     }
 }
