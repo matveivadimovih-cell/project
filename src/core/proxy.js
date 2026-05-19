00ea06bc305faca459ef9d9f11f8084d0aef076c
MTV <matveivadimovih@gmail.com>
2026-05-20 00:08:40 +0300

    modified:   src/core/proxy.js

--- src/core/proxy.js
+++ src/core/proxy.js
@@ -1,7 +1,36 @@
 import { ApiService } from "../serv/api";
 
-const PUBLIC_METHODS = ["login", "getMarketPrice", "getAllMarketPrices"];
-const PRIVATE_METHODS = ["logout", "getPortfolio", "getPortfolioSync", "executeMarketOrder", "placeLimitOrder", "cancelLimitOrder"];
-
 const apiService = new ApiService();
+
+const callTime = new Map();
+
+export function apiSecure()
+{
+    const proxyConsfid = {
+        authStrategy: "JWT",
+        rateLimitPerSecond: 5,
+        apiKey: null,
+    }
+
+    const PUBLIC_METHODS = ["login", "getMarketPrice", "getAllMarketPrices"];
+    const PRIVATE_METHODS = ["logout", "getPortfolio", "getPortfolioSync", "executeMarketOrder", "placeLimitOrder", "cancelLimitOrder"];
+
+    const handler = {
+        get(target, prop, receiver)
+        {
+            const value = target[prop];
+
+            if(typeof value !== "function")
+            {
+                return value;
+            }
+
+            return function(...args)
+            {
+
+            }
+        }
+    }
+
+}
 
