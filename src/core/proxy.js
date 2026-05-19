02d897c5d77ea02d14f1c81a05133b4d49ca659a
MTV <matveivadimovih@gmail.com>
2026-05-20 00:59:45 +0300

    modified:   src/core/proxy.js

--- src/core/proxy.js
+++ src/core/proxy.js
@@ -6,8 +6,8 @@ const callTime = new Map();
 
 export function apiSecure()
 {
-    const proxyConsfid = {
-        authStrategy: "JWT",
+    const proxyConfig = {
+        authStrategy: "JWT", // OAuth, API_Key, JWT
         rateLimitPerSecond: 5,
         apiKey: null,
     }
@@ -27,10 +27,43 @@ export function apiSecure()
 
             return function(...args)
             {
+                const now = Date.now();
+                if(!callTime.has(prop))
+                {
+                    callTime.set(prop, []);
+                }
 
+                const recentCalls = callTime.get(prop).filter((time) => now - time < 1000)
+                if(recentCalls.length >= proxyConfig.rateLimitPerSecond)
+                {
+                    throw new Error("too many requests");
+                }
+
+                recentCalls.push(now);
+                callTime.set(prop, recentCalls);
+
+                if(PRIVATE_METHODS.includes(prop))
+                {
+                    let isAuth = false;
+
+                    if(proxyConfig.authStrategy === "JWT" || proxyConfig.authStrategy === "OAuth")
+                    {
+                        isAuth = target.isAuthorized();
+                    }
+                    else if(proxyConfig.authStrategy === "API_Key")
+                    {
+                        isAuth = proxyConfig.apiKey === "good_key_1";
+                    }
+                    
+                    if(!isAuth)
+                    {
+                        throw new Error("Unauthorized");
+                    }
+                }
             }
         }
     }
 
-}
+    const apiProxy = new Proxy(apiService, handler);
 
+}
